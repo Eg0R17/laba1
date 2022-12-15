d33aac7512770a440f03075606a741268986c42c
Eg0R17 <117291267+Eg0R17@users.noreply.github.com>
2022-12-15 13:34:32 +0300

    Add files via upload

---
+++ 3laba/1. Two Sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = dict()
+        
+        for i in range(len(nums)):
+            delta = target - nums[i]
+            if delta in hashmap:
+                return [hashmap[delta], i]
+        
+            hashmap[nums[i]] = i
---
+++ 3laba/242. Valid Anagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+
+        sd= {}
+        for i in s:
+            if i not in sd:
+                sd[i]=1
+            else:
+                sd[i]+=1
+            
+        for i in t:
+            if i not in sd:
+                return False
+            else:
+                sd[i]-=1
+                if sd[i]<0:
+                    return False
+        return True
